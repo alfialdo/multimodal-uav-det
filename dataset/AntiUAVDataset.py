update: added anchors mechanism, fix issue in training script and loss calculation

dataset/AntiUAVDataset.py
@@ -13,12 +13,13 @@ from ._helper import load_json, load_attributes, connect_sftp, create_mosaic_4_i
 from utils.datatype import BatchData
 
 class AntiUAVDataset(Dataset):
-    def __init__(self, root_dir, transform, remote=None, mosaic=False):
+    def __init__(self, root_dir, transform, remote=None, mosaic=False, img_size=(640, 640)):
         self.data_set = os.path.basename(root_dir)
         self.remote = remote
         self.data = self.__load_data(root_dir)
         self.transform = transform
         self.mosaic = mosaic
+        self.img_size = img_size
 
     def __len__(self):
         return len(self.data) // 4
@@ -26,25 +27,29 @@ class AntiUAVDataset(Dataset):
 
     def __getitem__(self, idx):
         if self.mosaic:
-            total_img = 4
+            total_img = 10
             rows = self.data.sample(total_img)
 
             images = [self.__load_image(x) for x in rows.img_path]
             bboxes = rows.gt_rect.tolist()
-            labels = rows.exist.tolist()
-            img, bboxes = create_mosaic_4_img(images, bboxes)
+            img, bboxes = create_mosaic_4_img(images, bboxes, target_size=self.img_size)
+            labels = [1] * len(bboxes)
 
         else:
             row = self.data.iloc[idx]
             img = self.__load_image(row.img_path)
             bboxes = [row.gt_rect]
-            labels = [row.exist]
+            labels = [1]
 
         # Apply necessary transforms to the image
         results = self.transform(image=img, bboxes=bboxes, labels=labels)
         bbox = torch.tensor(results['bboxes']).squeeze(0)
             
-        return BatchData(image=results['image'], bbox=bbox, obj=torch.tensor(labels))
+        return BatchData(
+            image=results['image'], 
+            bbox=bbox, 
+            # obj=torch.tensor(labels)
+        )
 
     def __load_image(self, img_path):
 
@@ -98,7 +103,7 @@ class AntiUAVDataset(Dataset):
 
         # Filter images that not bounding box
         df = pd.concat(df, ignore_index=True)
-        df = df[df['exist'] == 1].reset_index(drop=True)
+        df = df[df.exist == 1].reset_index(drop=True)
 
         # Transform bbox to xyxy
         df['gt_rect'] = df.gt_rect.apply(lambda x: box_convert(torch.tensor(x), 'xywh', 'xyxy').squeeze(0))

dataset/_helper.py
@@ -106,22 +106,25 @@ def connect_sftp():
 
 
 
-def create_dataloader(dir_path, batch_size, shuffle=False, tsfm=None, remote=None, workers=4, mosaic=False):
+def create_dataloader(dir_path, batch_size, shuffle=False, tsfm=None, remote=None, workers=4, mosaic=False, img_size=(640, 640)):
     """
     Create a DataLoader for the AntiUAVDataset.
 
     Args:
-    dir_path (str): The root directory path for the dataset.
-    batch_size (int): The batch size for the DataLoader.
-    shuffle (bool, optional): Whether to shuffle the data. Defaults to False.
-    tsfm (callable, optional): A function/transform to apply to the image samples. Defaults to None.
-    remote (object, optional): A client for remote file access. If None, local file system is used. Defaults to None.
+        dir_path (str): The root directory path for the dataset.
+        batch_size (int): The batch size for the DataLoader.
+        shuffle (bool, optional): Whether to shuffle the data. Defaults to False.
+        tsfm (callable, optional): A function/transform to apply to the image samples. Defaults to None.
+        remote (object, optional): A client for remote file access. If None, local file system is used. Defaults to None.
+        workers (int, optional): Number of worker processes for data loading. Defaults to 4.
+        mosaic (bool, optional): Whether to use mosaic augmentation. Defaults to False.
+        img_size (tuple, optional): Target size (height, width) to resize images to. Defaults to (640, 640).
 
     Returns:
-    torch.utils.data.DataLoader: A DataLoader for the AntiUAVDataset.
+        torch.utils.data.DataLoader: A DataLoader for the AntiUAVDataset.
     """
     from .AntiUAVDataset import AntiUAVDataset
-    dataset = AntiUAVDataset(root_dir=dir_path, transform=tsfm, remote=remote, mosaic=mosaic)
+    dataset = AntiUAVDataset(root_dir=dir_path, transform=tsfm, remote=remote, mosaic=mosaic, img_size=img_size)
 
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=workers)
 
@@ -171,8 +174,8 @@ def plot_sample_data(dataloader):
 
 
 def create_mosaic_4_img(images, bboxes, target_size=(640, 640)):
-    if len(images) != 4 or len(bboxes) != 4:
-        raise ValueError("Exactly 4 images and 4 sets of bounding boxes are required to create a mosaic.")
+    if len(images) < 4 or len(images) != len(bboxes):
+        raise ValueError("Need at least 4 images and 4 sets of bounding boxes to create a mosaic.")
     
     # Create a new blank image
     mosaic = np.zeros((target_size[0], target_size[1], 3), dtype=np.uint8)
@@ -184,18 +187,13 @@ def create_mosaic_4_img(images, bboxes, target_size=(640, 640)):
     updated_bboxes = []
     
     # Resize and paste each image into the mosaic
-    for i, (img, bbox) in enumerate(zip(images, bboxes)):
-        # Resize the image
+    i = 0
+    for img, bbox in zip(images, bboxes):
+        # Get original image size and calculate position
         original_size = img.shape[:2]
-        img_resized = cv2.resize(img, (img_width, img_height), interpolation=cv2.INTER_LANCZOS4)
-        
-        # Calculate position
         x = (i % 2) * img_width
         y = (i // 2) * img_height
-        
-        # Paste the image
-        mosaic[y:y+img_height, x:x+img_width] = img_resized
-        
+
         # Update bounding box coordinates
         scale_x = img_width / original_size[1]
         scale_y = img_height / original_size[0]
@@ -205,6 +203,19 @@ def create_mosaic_4_img(images, bboxes, target_size=(640, 640)):
         y1 = y + (y1 * scale_y)
         x2 = x + (x2 * scale_x)
         y2 = y + (y2 * scale_y)
+
+        if x1 >= x2 or y1 >= y2:
+            continue
+
         updated_bboxes.append([x1, y1, x2, y2])
+    
+        # Resize and paste the image to placeholder
+        img_resized = cv2.resize(img, (img_width, img_height), interpolation=cv2.INTER_LANCZOS4)
+        mosaic[y:y+img_height, x:x+img_width] = img_resized
+
+        if len(updated_bboxes) >= 4:
+            return mosaic, updated_bboxes
+            
+        i += 1
         
-    return mosaic, updated_bboxes
+    

model/RTMUAVDet.py
@@ -2,12 +2,13 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 import pytorch_lightning as pl
+from torchvision.ops import box_iou, box_convert
 
 from typing import List
 import einops
 
 from utils.datatype import DetectionResults, BatchData
-from utils.metrics import iou_loss, cross_entropy_loss
+from utils.metrics import bbox_loss, objectness_loss
 
 # Custom Module
 class ConvModule(pl.LightningModule):
@@ -219,6 +220,7 @@ class ObjectnessHead(pl.LightningModule):
         predict_c = n_anchors * 1
         self.n_anchors = n_anchors
         self.conv_bbox = nn.Conv2d(in_channels, predict_c, kernel_size=(1,1), stride=(1,1))
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.conv_bbox(x)
@@ -228,6 +230,8 @@ class ObjectnessHead(pl.LightningModule):
             n_anchors=self.n_anchors, obj=1
         )
 
+        x = self.sigmoid(x)
+
         return x
     
 class BBoxHead(pl.LightningModule):
@@ -236,6 +240,7 @@ class BBoxHead(pl.LightningModule):
         predict_c = n_anchors * 4
         self.n_anchors = n_anchors
         self.conv_obj = nn.Conv2d(in_channels, predict_c, kernel_size=(1,1), stride=(1,1))
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.conv_obj(x)
@@ -244,14 +249,19 @@ class BBoxHead(pl.LightningModule):
             n_anchors=self.n_anchors, bbox=4
         )
 
+        x = self.sigmoid(x)
+
         return x
     
 
 class RTMHead(pl.LightningModule):
-    def __init__(self, x_c_in:list, n_anchors):
+    def __init__(self, x_c_in:list, anchors:list, det_scales:list):
         super().__init__()
 
+        self.det_scales = det_scales
         self.detection_head = nn.ModuleList()
+        self.anchors = anchors
+        n_anchors = len(anchors[0])
 
         for in_channels in x_c_in:
             self.detection_head.append(nn.ModuleDict(dict(
@@ -259,30 +269,59 @@ class RTMHead(pl.LightningModule):
                 bbox=BBoxHead(in_channels, n_anchors)
             )))
 
+    
+    def __calculate_bbox_size(self, batch_size, h, w, bbox, anchors):
+        # Setup grid coordnates for offsets
+        device = bbox.device
+        grid_x = torch.arange(w).repeat(batch_size, len(anchors), w, 1).to(device)
+        grid_y = torch.arange(h).repeat(batch_size, len(anchors), h, 1).transpose(2, 3).to(device)
+
+        # Setup anchor size
+        anchor_w = einops.rearrange(anchors[:, 0], 'size -> 1 size 1 1').to(device)
+        anchor_h = einops.rearrange(anchors[:, 1], 'size -> 1 size 1 1').to(device)
+
+        # Calculate bbox coordinates relative to grid
+        px = (bbox[..., 0] * 2 - 0.5 + grid_x)
+        py = (bbox[..., 1] * 2 - 0.5 + grid_y)
+        pw = (bbox[..., 2] * 2) ** 2 * anchor_w
+        ph = (bbox[..., 3] * 2) ** 2 * anchor_h
+        bbox = torch.stack([px, py, pw, ph], dim=-1)
+        
+        return bbox
+    
     def forward(self, x1, x2):
         f_maps = [x1, x2]
         outs = []
 
-        for f_map, det_head in zip(f_maps, self.detection_head):
-            outs.append(DetectionResults(
-                # Set output shape for prediction
-                obj=einops.rearrange(det_head['obj'](f_map), 'b n_anchors h w obj -> b (n_anchors h w) obj'),
-                bbox=einops.rearrange(det_head['bbox'](f_map), 'b n_anchors h w bbox -> b (n_anchors h w) bbox')
-            ))
+        for head_idx, (f_map, det_head) in enumerate(zip(f_maps, self.detection_head)):
+            scale = einops.parse_shape(f_map, 'b n_anchors h w')
+            obj = det_head['obj'](f_map)
+            bbox = det_head['bbox'](f_map)
+
+            # Calculate bbox coordinates relative to grid
+            bbox = self.__calculate_bbox_size(
+                scale['b'], scale['h'], scale['w'], 
+                bbox, self.anchors[head_idx]
+            )
+
+            outs.append(DetectionResults(obj=obj, bbox=bbox))
         
         return outs
 
 
 class RTMUAVDet(pl.LightningModule):
-    def __init__(self, img_channels, n_anchors, learning_rate):
+    def __init__(self, input_size, anchors, learning_rate, optimizer='Adam', det_scales=[160, 80]):
         super().__init__()
 
         self.learning_rate = learning_rate
+        self.optimizer = optimizer
+        self.input_size = input_size
+        self.det_scales = det_scales
 
         self.backbone = nn.ModuleDict(dict(
             MDyCSP_1=nn.Sequential(
                 # TODO: increase stem layer output channel?
-                StemLayer(img_channels, 32),
+                StemLayer(input_size[0], 32),
                 MDyCSPModule(in_channels=32, out_channels=128, dy_channel_size=128),
             ),
             MDyCSP_2=MDyCSPModule(in_channels=128, out_channels=256)
@@ -290,7 +329,7 @@ class RTMUAVDet(pl.LightningModule):
 
         self.neck = MFDFEncoderModule(x1_c_in=128, x2_c_in=256)
 
-        self.head = RTMHead(x_c_in=[128, 256], n_anchors=n_anchors)
+        self.head = RTMHead(x_c_in=[128, 256], anchors=anchors, det_scales=det_scales)
     
     def forward(self, x):
         x1 = self.backbone['MDyCSP_1'](x)
@@ -304,27 +343,77 @@ class RTMUAVDet(pl.LightningModule):
         return outs
     
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        if self.optimizer == 'SGD':
+            optimizer = torch.optim.SGD(self.parameters(), lr=self.learning_rate)
+        elif self.optimizer == 'Adam':
+            optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        else:
+            raise ValueError(f"Invalid optimizer: {self.optimizer}")
+
         return optimizer
 
-    def compute_loss(self, logits:List[DetectionResults], batch:BatchData):
-        total_loss = 0
+    def compute_loss(self, outs:List[DetectionResults], batch:BatchData):
+        
+        device = outs[0].bbox.device
+        total_loss = torch.tensor(0.0).to(device)
 
-        for det in logits:
-            total_loss += (iou_loss(det.bbox, batch.bbox) + cross_entropy_loss(det.obj, batch.obj))
+        for i, det in enumerate(outs):
+            # Prepare preds and targets
+            p_bbox, p_obj = self.__prepare_preds(det)
+            t_bbox = self.__scale_targets(batch.bbox, self.det_scales[i], device)
 
-        return (total_loss / len(logits))
+            # Filter high IoU bboxes
+            filtered_p_bbox, t_obj = self.__filter_high_iou_bboxes(p_bbox, t_bbox)
+            total_loss += (bbox_loss(filtered_p_bbox, t_bbox) + objectness_loss(p_obj, t_obj))
+
+
+        return total_loss
 
     def training_step(self, batch:BatchData, batch_idx):
-        logits = self.forward(batch.image)
-        loss = self.compute_loss(logits, batch)
+        outs = self.forward(batch.image)
+        loss = self.compute_loss(outs, batch)
         self.log('train_loss', loss, prog_bar=True, batch_size=len(batch))
 
         return loss
 
     def validation_step(self, batch:BatchData, batch_idx):
-        logits = self.forward(batch.image)
-        loss = self.compute_loss(logits, batch)
+        outs = self.forward(batch.image)
+        loss = self.compute_loss(outs, batch)
         self.log('val_loss', loss, on_epoch=True, prog_bar=True, batch_size=len(batch))
 
         return loss
+    
+    def __filter_high_iou_bboxes(self, preds, targets, iou_th=0.5):
+        # Calculate IoU matrix
+        # preds_xyxy = box_convert(preds, in_fmt='cxcywh', out_fmt='xyxy')
+        # targets_xyxy = box_convert(targets, in_fmt='cxcywh', out_fmt='xyxy')
+        device = preds.device
+        ious = box_iou(preds.squeeze(), targets.squeeze()) 
+        
+        # Get maximum IoU for each prediction
+        max_ious, _ = torch.max(ious, dim=1)  # (19200,)
+
+        # Filter IoU than less than threshold (0.5)
+        filter_iou = max_ious > iou_th
+        filtered_preds = preds[:, filter_iou, :]
+        
+        # Create objectness matrix (0 or 1)
+        objectness = torch.zeros_like(max_ious).to(device)
+        objectness[filter_iou] = 1.0
+        
+        return filtered_preds, objectness
+    
+    def __scale_targets(self, targets, det_scale, device):
+        # targets = box_convert(targets, in_fmt='xyxy', out_fmt='cxcywh')
+        scale_factor = self.input_size[1] // det_scale
+        scaled_targets = targets / scale_factor
+
+        return scaled_targets.to(device)
+    
+    def __prepare_preds(self, det:DetectionResults):
+        p_bbox = einops.rearrange(det.bbox, 'b n_anchors h w bbox -> b (n_anchors h w) bbox')
+        p_obj = einops.rearrange(det.obj, 'b n_anchors h w obj -> b (n_anchors h w) obj')
+
+        p_bbox = box_convert(p_bbox, in_fmt='cxcywh', out_fmt='xyxy')
+
+        return p_bbox, p_obj

train.py
@@ -1,9 +1,11 @@
 from model.RTMUAVDet import RTMUAVDet
 from dataset import create_dataloader
 
-from torchvision import transforms as T
+import torch
 import pytorch_lightning as pl
 from pytorch_lightning import seed_everything
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 import hydra
 from omegaconf import OmegaConf
@@ -16,19 +18,21 @@ def get_dataloader(**kwargs):
     remote = kwargs.get('remote')
     img_size = kwargs.get('img_size', (640, 640))
     test = kwargs.get('test', False)
+    mosaic = kwargs.get('mosaic', False)
 
-    tsfm = T.Compose([
-        T.Resize(size=img_size), # Resize to img_size
-        T.ToTensor(),
-        T.Lambda(lambda x: x / 255.0)  # Scale pixel values to range 0-1
-    ])
+    tsfm = A.Compose([
+        A.Resize(img_size[0], img_size[1]),
+        A.Affine(scale=(0.8, 1.2), translate_percent=(-0.1, 0.1), rotate=(-30, 30), shear=(-15, 15)),
+        A.ToFloat(max_value=255.0), 
+        ToTensorV2(),
+    ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels']))
 
     if test:
-        test_loader = create_dataloader(dir_path=os.path.join(root_dir, "test"), batch_size=batch_size, shuffle=False, remote=remote, img_size=img_size)
+        test_loader = create_dataloader(dir_path=os.path.join(root_dir, "test"), batch_size=batch_size, remote=remote, img_size=img_size)
         return test_loader
     
-    train_loader = create_dataloader(dir_path=os.path.join(root_dir, "train"), tsfm=tsfm, batch_size=batch_size, shuffle=True, remote=remote, img_size=img_size)
-    val_loader = create_dataloader(dir_path=os.path.join(root_dir, "val"), tsfm=tsfm, batch_size=batch_size, shuffle=False, remote=remote, img_size=img_size)
+    train_loader = create_dataloader(dir_path=os.path.join(root_dir, "train"), tsfm=tsfm, batch_size=batch_size, remote=remote, img_size=img_size, mosaic=mosaic)
+    val_loader = create_dataloader(dir_path=os.path.join(root_dir, "val"), tsfm=tsfm, batch_size=batch_size, remote=remote, img_size=img_size, mosaic=mosaic)
 
     return train_loader, val_loader
 
@@ -50,11 +54,13 @@ def train(config):
         batch_size=dataset_cfg.batch_size,
         remote=dataset_cfg.remote,
         img_size=dataset_cfg.image_size,
-        workers=dataset_cfg.workers
+        workers=dataset_cfg.workers,
+        mosaic=dataset_cfg.mosaic
     )
 
     if config.train.model == "RTMUAVDet":
-        model = RTMUAVDet(img_channels=3, n_anchors=hparams.n_anchors, learning_rate=hparams.lr)
+        anchors = torch.tensor(hparams.anchors)
+        model = RTMUAVDet(input_size=trainer_cfg.input_size, anchors=anchors, learning_rate=hparams.lr, optimizer=hparams.optim, det_scales=hparams.det_scales)
     else:
         raise ValueError(f"Model {config.train.model} not supported")
 

utils/metrics.py
@@ -1,20 +1,39 @@
-# TODO: add metrics to calculate AP & AR (0.5 - 0.95), FPS, GFLOPS
+# TODO: add metrics to calculate FPS, GFLOPS
 import torch
 from torchvision.ops import box_convert, complete_box_iou_loss
+from torchmetrics.detection import MeanAveragePrecision
 import torch.nn.functional as F
 import einops
 
-def iou_loss(pred, target, reduction='mean'):
-    target = einops.rearrange(target, 'b xyxy -> b 1 xyxy')
 
-    return complete_box_iou_loss(pred, target, reduction=reduction)
+def bbox_loss(preds, targets):
 
-def cross_entropy_loss(pred, target):
-    target = einops.rearrange(target, 'obj -> obj 1')
-    return F.cross_entropy(pred, target)
+    # preds = box_convert(preds, in_fmt='cxcywh', out_fmt='xyxy')
 
+    losses = []
 
-from torchmetrics.detection import MeanAveragePrecision
+    for t_bbox in targets.squeeze():
+        # TODO: ciou loss calculation method
+        if preds.size(1) != 0:
+            loss = complete_box_iou_loss(preds.squeeze(), t_bbox).min()
+            losses.append(loss)
+        else:
+            continue
+    
+    if len(losses) == 0:
+        avg_loss = torch.tensor(0.0)
+    else:
+        avg_loss = torch.tensor(losses).mean()
+
+    return avg_loss
+
+def objectness_loss(preds, targets, reduction='mean'):
+    # Ensure target is on same device as pred
+    preds = einops.rearrange(preds, 'b total_pred obj -> (b total_pred obj)')
+    avg_loss = F.binary_cross_entropy(preds, targets, reduction=reduction)
+
+    return avg_loss
+
 
 def calculate_ap_ar_single_class(preds, targets):
     """
@@ -24,7 +43,7 @@ def calculate_ap_ar_single_class(preds, targets):
     :param targets: List of dictionaries, each containing 'boxes'
     :return: Dictionary containing AP and AR metrics
     """
-    # Add dummy labels for single class (0)
+    # Add dummy labels for single class (0)r
     for pred in preds:
         pred['labels'] = torch.zeros_like(pred['scores'], dtype=torch.int64)
     for target in targets:
@@ -49,4 +68,48 @@ def calculate_ap_ar_single_class(preds, targets):
     }
 
 
+# def bbox_ciou(box1, box2, eps=1e-7):
+#     """
+#     Calculate CIoU loss between two bounding boxes
+#     Args:
+#         box1: Predictions (N, 4) in (cx, cy, w, h) format
+#         box2: Ground truth (N, 4) in (cx, cy, w, h) format
+#     """
+#     # Convert from (cx, cy, w, h) to (x1, y1, x2, y2)
+#     b1_x1, b1_x2 = box1[..., 0] - box1[..., 2] / 2, box1[..., 0] + box1[..., 2] / 2
+#     b1_y1, b1_y2 = box1[..., 1] - box1[..., 3] / 2, box1[..., 1] + box1[..., 3] / 2
+#     b2_x1, b2_x2 = box2[..., 0] - box2[..., 2] / 2, box2[..., 0] + box2[..., 2] / 2
+#     b2_y1, b2_y2 = box2[..., 1] - box2[..., 3] / 2, box2[..., 1] + box2[..., 3] / 2
+
+#     # Intersection area
+#     inter = torch.clamp((torch.min(b1_x2, b2_x2) - torch.max(b1_x1, b2_x1)), min=0) * \
+#             torch.clamp((torch.min(b1_y2, b2_y2) - torch.max(b1_y1, b2_y1)), min=0)
+
+#     # Union Area
+#     w1, h1 = b1_x2 - b1_x1, b1_y2 - b1_y1
+#     w2, h2 = b2_x2 - b2_x1, b2_y2 - b2_y1
+#     union = (w1 * h1 + eps) + w2 * h2 - inter
+
+#     iou = inter / union
+
+#     # Calculate the smallest enclosing box
+#     cw = torch.max(b1_x2, b2_x2) - torch.min(b1_x1, b2_x1)  # Convex width
+#     ch = torch.max(b1_y2, b2_y2) - torch.min(b1_y1, b2_y1)  # Convex height
+
+#     # Calculate diagonal distance
+#     c2 = cw ** 2 + ch ** 2 + eps
+#     rho2 = ((box2[..., 0] - box1[..., 0]) ** 2 +
+#             (box2[..., 1] - box1[..., 1]) ** 2)  # Center dist squared
+
+#     # Calculate aspect ratio consistency term
+#     v = (4 / (math.pi ** 2)) * \
+#         torch.pow(torch.atan(w2 / (h2 + eps)) - torch.atan(w1 / (h1 + eps)), 2)
+    
+#     # Calculate alpha term for aspect ratio consistency
+#     alpha = v / (1 - iou + v + eps)
+
+#     # Calculate CIoU
+#     ciou = iou - (rho2 / c2 + v * alpha)
+    
+#     return ciou
 